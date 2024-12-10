8e8c00f8a7408f1321f80272c808c8e8d6806798
Ersin Uluağaç <ersinuluagac@gmail.com>
2024-12-10 23:23:53 +0300

    Created repository methods

---
+++ ToDoList.Repository/Abstracts/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Core.Models;
+
+namespace ToDoList.Repository.Abstracts
+{
+    public interface IRepository<T> where T : BaseEntity 
+    {
+        int Add(T entity);
+        int Update(T entity);
+        int Delete(T entity);
+        ICollection<T> GetAll();
+        ICollection<T> GetById(int id);
+    }
+}
---
+++ ToDoList.Repository/Concretes/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Core.Models;
+using ToDoList.Repository.Abstracts;
+
+namespace ToDoList.Repository.Concretes
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        public int Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+        public int Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+        public int Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+        public ICollection<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public ICollection<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
