388b6c4969381869b3e1c4a804f39c21ab6768b2
uluagac <ersinuluagac@gmail.com>
2024-12-13 00:29:49 +0300

    restart repository

---
+++ ToDoList.Repository/Abstracts/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Core.Models;
+
+namespace ToDoList.Repository.Abstracts
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        int Add(T entity);
+        int Update(T entity);
+        int Delete(T entity);
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+    }
+}
---
+++ ToDoList.Repository/Concretes/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Core.Models;
+using ToDoList.Repository.Abstracts;
+
+namespace ToDoList.Repository.Concretes
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+    }
+}
