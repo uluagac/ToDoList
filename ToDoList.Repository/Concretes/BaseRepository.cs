caaec69d6035f79c38645d8de0f98db74d63f35d
uluagac <ersinuluagac@gmail.com>
2024-12-13 22:49:41 +0300

    base repository, crud.

--- ToDoList.Repository/Concretes/BaseRepository.cs
+++ ToDoList.Repository/Concretes/BaseRepository.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoList.Core.Contexts;
 using ToDoList.Core.Models;
 using ToDoList.Repository.Abstracts;
 
@@ -10,5 +12,39 @@ namespace ToDoList.Repository.Concretes
 {
     public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
     {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public int Add(T entity)
+        {
+            _context.Add(entity);
+            return _context.SaveChanges();
+        }
+        public int Update(T entity)
+        {
+            _context.Update(entity);
+            return _context.SaveChanges();
+        }
+        public int Delete(T entity)
+        {
+            _context.Remove(entity);
+            return _context.SaveChanges();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+
     }
 }
