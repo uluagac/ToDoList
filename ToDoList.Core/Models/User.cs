dab01e68051e49ad5c5d02687053a4ad7d1df6ff
Ersin Uluağaç <ersinuluagac@gmail.com>
2024-12-15 10:42:19 +0300

    User encapsulation

--- ToDoList.Core/Models/User.cs
+++ ToDoList.Core/Models/User.cs
@@ -8,10 +8,62 @@ namespace ToDoList.Core.Models
 {
     public class User : BaseEntity
     {
+        // Fields
+        private string _email;
+        private string _username;
+        private string _password;
+
+        // Constructor
+        public User()
+        {
+
+        }
+
         // Properties
-        public string Email { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                if (value.Contains("@") && value.Contains("."))
+                    _email = value.Trim();
+                throw new ArgumentException("Girilen değer bir e-posta gerekliliklerini içermiyor!");
+            }
+        }
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (value.Contains(" "))
+                    throw new ArgumentException("Kullanıcı adı boşluk içeremez!");
+                _username = value.Trim();
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (value.Contains(" "))
+                    throw new ArgumentException("Parola boşluk içeremez!");
+                else
+                {
+                    if (value.Length >= 6 && value.Any(char.IsDigit) && value.Any(char.IsLower) && value.Any(char.IsUpper))
+                        _password = value.Trim();
+                    throw new ArgumentException("Parola en az bir rakam, bir küçük harf ve bir büyük harf içermelidir!");
+                }
+            }
+        }
 
         // Navigation Properties
         public ICollection<ToDoTask> ToDoTasks { get; set; } = new List<ToDoTask>();
