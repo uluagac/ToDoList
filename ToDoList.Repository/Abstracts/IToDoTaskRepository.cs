4805c43f09bfabaf6ec421d0091e25fb1a0446d8
uluagac <ersinuluagac@gmail.com>
2024-12-20 21:51:15 +0300

    add methods

--- ToDoList.Repository/Abstracts/IToDoTaskRepository.cs
+++ ToDoList.Repository/Abstracts/IToDoTaskRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoList.Core.Enums;
 using ToDoList.Core.Models;
 
 namespace ToDoList.Repository.Abstracts
@@ -10,7 +11,7 @@ namespace ToDoList.Repository.Abstracts
     public interface IToDoTaskRepository : IBaseRepository<ToDoTask>
     {
         IEnumerable<ToDoTask> GetUserTasks(int userId);
-        IEnumerable<ToDoTask> GetTasksByStatus(int userId, TaskStatus status);
+        IEnumerable<ToDoTask> GetTasksByStatus(int userId, ToDoTaskStatus status);
         IEnumerable<ToDoTask> GetTasksByDueDate(int userId, DateTime dueDate);
         ToDoTask GetTaskWithSubTasks(int taskId);
     }
--- ToDoList.Repository/Concretes/ToDoTaskRepository.cs
+++ ToDoList.Repository/Concretes/ToDoTaskRepository.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoList.Core.Contexts;
+using ToDoList.Core.Enums;
 using ToDoList.Core.Models;
 using ToDoList.Repository.Abstracts;
 
@@ -10,5 +13,29 @@ namespace ToDoList.Repository.Concretes
 {
     public class ToDoTaskRepository : BaseRepository<ToDoTask>, IToDoTaskRepository
     {
+        private readonly DbSet<ToDoTask> _dbSet;
+
+        public ToDoTaskRepository(AppDbContext context) : base(context)
+        {
+            _dbSet = context.Set<ToDoTask>();
+        }
+
+        public IEnumerable<ToDoTask> GetUserTasks(int userId)
+        {
+            return _dbSet.Where(task => task.AssignedUserId == userId).ToList();
+        }
+        public IEnumerable<ToDoTask> GetTasksByStatus(int userId, ToDoTaskStatus status)
+        {
+            return _dbSet.Where(task => task.AssignedUserId == userId && task.Status == status).ToList();
+        }
+        public IEnumerable<ToDoTask> GetTasksByDueDate(int userId, DateTime dueDate)
+        {
+            return _dbSet.Where(task => task.AssignedUserId == userId && task.DueDate == dueDate).ToList();
+        }
+        public ToDoTask GetTaskWithSubTasks(int taskId)
+        {
+            return _dbSet.Include(task => task.SubTasks).FirstOrDefault(task => task.Id == taskId);
+        }
+
     }
 }
